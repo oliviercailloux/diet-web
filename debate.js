6c9b93576bfa8d278f82da11ce70a08efba2db75
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-06-08 23:55:03 +0200

    Various

---
+++ debate.js
@@ -0,0 +1,5 @@
+const accept = document.getElementById('accept-conditions');
+accept.onclick = function () {
+    console.log('Conditions accepted.');
+    
+}
--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-function createParagraph() {
-  let para = document.createElement('p');
-  para.textContent = 'You clicked the button.';
-  document.body.appendChild(para);
+function makeId(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 }
 
-const buttons = document.querySelectorAll('button');
-
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', createParagraph);
+if (window.location.protocol !== 'https:' && location.hostname !== "localhost" && location.hostname !== "127.0.0.1") {
+  throw new Error('Protocol should be https.');
 }
+
+l = window.localStorage;
+console.log(l);
+
+if (l.getItem('id') === null) {
+  d = new Date();
+  l.setItem('id', `${d.toISOString()} - ${makeId(5)}`);
+  l.setItem('pw', `${makeId(5)} ${makeId(5)} ${makeId(5)} ${makeId(5)} ${makeId(5)}`);
+}
+const id = l.getItem('id');
+
+console.log(`Using id ${id}.`);
