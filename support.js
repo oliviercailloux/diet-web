f8b81098c38e00f1701c8f597aed71968410643e
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2021-06-21 11:23:16 +0200

    Font-awesome; debate attempt

---
+++ support.js
@@ -0,0 +1,8 @@
+function makeId(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
